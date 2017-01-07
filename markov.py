1cc724d7d20e50f138f9162a17db625192303f16
Anthony Bloomer <ant0@protonmail.ch>
2017-01-07 03:03:51 +0000

    Update markov.py

--- markov.py
+++ markov.py
@@ -1,9 +1,9 @@
 import markovify
 import schedule
 import time
-import random
 import tweepy
 import config
+import csv
 
 try:
     import HTMLParser
@@ -15,40 +15,38 @@ try:
 except ImportError:
     htmlparser = None
 
-hashtags = [
-    '#MAGA',
-    '#MakeAmericaGreatAgain',
-    '#ImWithYou',
-    '#AlwaysTrump',
-    '#AmericaFirst',
-    '#Trump2016',
-    '#TrumpPence16',
-    '#PresidentTrump'
-]
-
 auth = tweepy.OAuthHandler(config.consumer_key, config.consumer_secret)
 auth.set_access_token(config.access_key, config.access_secret)
 api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
 
 def main():
-    speeches()
-    schedule.every(10).minutes.do(speeches)
+    post()
+    schedule.every(10).minutes.do(post)
     while 1:
         schedule.run_pending()
         time.sleep(1)
 
 
-def speeches():
-    f = open('speeches.txt', 'r')
-    text_model = markovify.Text(f.read())
-    tweet = text_model.make_short_sentence(100) + ' ' + random.choice(hashtags)
+def post():
+    with open('realDonaldTrump_tweets.csv', 'rb') as csvfile:
+        reader = csv.reader(csvfile)
+        text = ''
+        for row in reader:
+            escapes = ''.join([chr(char) for char in range(1, 32)])
+            t = str(row).translate(None, escapes)
+            t = t.decode('string_escape')
+            text += t
+
+    text_model = markovify.Text(text)
+
+    tweet = text_model.make_short_sentence(140)
+
     print('Posting to Twitter...')
     try:
         api.update_status(HTMLParser.HTMLParser().unescape(tweet))
     except ImportError:
         api.update_status(htmlparser.HTMLParser().unescape(tweet))
-    f.close()
     print('Tweet you posted: ' + tweet)
 
 
