82095fb66f88d46347235f6ae996afffc530ff01
twittermalala <twittermalala@users.noreply.github.com>
2017-02-25 17:18:12 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # Twitter application settings
-consumer_key = ''
-consumer_secret = ''
-access_key = ''
-access_secret = ''
+consumer_key = 'cpWYEt0rexuvUwmusM5r8lc3Y'
+consumer_secret = 'hKAUHxBClubaEKkylGcJpoXgfWLGDQIHxC8p64gy5RqXdSmVhH'
+access_key = '835480520721121280-x8k1F3ZMHYvSTSfHArrxd80NoTKuFtS'
+access_secret = 'xzFLK7U4YBfx1MlETevExBOMJQg42JhNAtdG1hmOd0bSp'
